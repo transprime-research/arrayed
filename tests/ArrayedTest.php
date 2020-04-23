b85405bb537b222cce0670455599396e59ea4452
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2020-04-23 21:42:42 +0300

    Test reduce() method

--- tests/ArrayedTest.php
+++ tests/ArrayedTest.php
@@ -14,18 +14,13 @@ class ArrayedTest extends TestCase
 
     public function testSum()
     {
-        $this->assertEquals(3, arrayed(1, 2)->sum()->done());
+        $this->assertEquals(3, arrayed(1, 2)->sum());
         $this->assertEquals(
             5,
             arrayed(1, 2, 'ninja')
                 ->filter(fn($val) => is_int($val))
                 ->map(fn($val) => $val + 1)
-                ->sum()()
-        );
-
-        $this->assertEquals(
-            7,
-            arrayed(1, 2)->map(fn($i) => $i + 1)->reduce(fn($v, $i) => $v + $i, 2)()
+                ->sum()
         );
     }
 
@@ -36,7 +31,22 @@ class ArrayedTest extends TestCase
             arrayed([1, 2, 'ninja'])
                 ->filter(fn($val) => is_int($val))
                 ->map(fn($val) => $val + 1)
-                ->sum()()
+                ->sum()
+        );
+    }
+
+    public function testReduce()
+    {
+        $this->assertEquals(
+            6,
+            arrayed([1, 2, 3])->reduce(fn($acc, $item) => $acc + $item, 0)()
+        );
+
+        $this->assertEquals(
+            7,
+            arrayed(1, 2)
+                ->map(fn($i) => $i + 1)
+                ->reduce(fn($v, $i) => $v + $i, 2)()
         );
     }
 
